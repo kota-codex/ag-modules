a377f06c35b6f7a7008b02dfdc5834546928e785
Andrey Kalmatskiy <andrewkoder82@gmail.com>
2025-10-02 23:01:41 -0400

    Make buildable on osx

--- build-apple.sh
+++ build-apple.sh
@@ -1,20 +1,17 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-# Update submodules
-git submodule update --init --recursive
-
 : "${VCPKG_ROOT:?Error: VCPKG_ROOT must be set to your vcpkg installation path}"
 
 # Collect extern modules
-MODULES=$(find extern -maxdepth 1 -mindepth 1 -type d -printf "%f\n")
+MODULES=$(find extern -maxdepth 1 -mindepth 1 -type d -exec basename {} \;)
 
 # Triples as: triple;arch;sysroot
 TRIPLES=(
   "x64-osx;x86_64;macosx"
   "arm64-osx;arm64;macosx"
-  "x64-ios;x86_64;iphonesimulator"
-  "arm64-ios;arm64;iphoneos"
+  #"x64-ios;x86_64;iphonesimulator" TODO:
+  #"arm64-ios;arm64;iphoneos"  TODO:
 )
 
 CONFIGS=(Release Debug)
@@ -30,7 +27,7 @@ for module in $MODULES; do
       echo "Building module ${module} for triple: ${triple}, config: ${config} in ${BUILD_DIR}"
       mkdir -p "${BUILD_DIR}"
 
-      cmake -S "external/${module}" -B "${BUILD_DIR}" -G "${GENERATOR}" \
+      cmake -S "extern/${module}" -B "${BUILD_DIR}" -G "${GENERATOR}" \
         -DCMAKE_TOOLCHAIN_FILE="${VCPKG_ROOT}/scripts/buildsystems/vcpkg.cmake" \
         -DVCPKG_TARGET_TRIPLET="${triple}" \
         -DCMAKE_BUILD_TYPE="${config}" \
